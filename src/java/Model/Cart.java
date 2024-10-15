c5ecd257650a10f95f88c589b94ee50170fde2ff
KhangKJ1502 <truongvankhangthanthanh@gmail.com>
2024-10-15 20:22:45 +0700

    Luu Len cho Chac

---
+++ src/java/Model/Cart.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Truong Van Khang - CE181852
+ */
+public class Cart {
+
+    private List<Item> items;
+
+    public Cart() {
+    }
+
+    public Cart(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    private Item getItemById(int id) {
+        for (Item i : items) {
+            if (i.getProduct().getProduct_id() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public int getQuantityById(int id) {
+        return getItemById(id).getQuantity();
+    }
+
+    public void addItem(Item t) {
+        if (getItemById(t.getProduct().getProduct_id()) != null) {
+            Item m = getItemById(t.getProduct().getProduct_id());
+            m.setQuantity(m.getQuantity() + t.getQuantity());
+        } else {
+            items.add(t);
+        }
+    }
+
+    public void removeItem(int id) {
+        if (getItemById(id) != null) {
+            items.remove(getItemById(id));
+        }
+    }
+
+    public double getTotalMoney() {
+        double t = 0;
+        for (Item i : items) {
+            t += (i.getQuantity() * i.getProduct().getPrice());
+        }
+        return t;
+    }
+
+}
--- src/java/Model/Item.java
+++ src/java/Model/Item.java
@@ -2,41 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package Model;
 
 /**
  *
  * @author Truong Van Khang - CE181852
  */
-public class cartItem {
-    private int id;
-    private String img ;
-    private double price ;
+public class Item {
+
+    private Product product;
+    private double price;
     private int quantity;
 
-    public cartItem(int id, String img, double price, int quantity) {
-        this.id = id;
-        this.img = img;
+    public Item() {
+
+    }
+
+    public Item(Product product, double price, int quantity) {
+        this.product = product;
         this.price = price;
         this.quantity = quantity;
-
     }
 
-    public int getId() {
-        return id;
+    public Product getProduct() {
+        return product;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
+    public void setProduct(Product product) {
+        this.product = product;
     }
 
     public double getPrice() {
